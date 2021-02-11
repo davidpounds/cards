13ac7a5421be0d83a895de855f5a60c14b8c58fc
David Pounds <david@davidpounds.com>
2021-02-11 17:43:05 +0000

    Adds top and bottom value and symbol to cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,18 @@
 import './Card.css';
 
 const Card = props => {
-  const { colour, suit, symbol, value, name, } = props;
+  const { colour, symbol, name } = props;
   const abbr = /\d+/.test(name) ? name : name.substr(0, 1);
 
-  return <div className={colour}>
-    {symbol} {abbr}
+  return <div className={`card ${colour}`}>
+    <AbbreviationAndSymbol abbr={abbr} symbol={symbol} position="top" />
+    <AbbreviationAndSymbol abbr={abbr} symbol={symbol} position="bottom" />
   </div>
+};
+
+const AbbreviationAndSymbol = props => {
+  const { abbr, symbol, position = 'top' } = props;
+  return <div className={`abbr-and-symbol ${position}`}>{abbr}{symbol}</div>;
 };
 
 export default Card;
