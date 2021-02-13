40fcc629a57cb1cce9cb43481f9dcd2e2159a6bb
David Pounds <david@davidpounds.com>
2021-02-13 00:10:12 +0000

    Adds a shuffle function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 import './App.css';
 import DECK from './data/deck';
 import Card from './components/Card';
 import DeckSvgInline from './components/DeckSvgInline';
 
 function App() {
+
+  const [deck, setDeck] = useState(DECK);
+
+  const shuffleDeck = () => {
+    const shuffledDeck = [...deck].sort((a, b) => Math.random() - 0.5);
+    setDeck(shuffledDeck);
+  };
+
   return (
     <div className="app">
-      {DECK.map(card => <Card {...card} />)}
+      {deck.map(card => <Card {...card} />)}
+      <button onClick={shuffleDeck}>Shuffle</button>
       <DeckSvgInline />
     </div>
   );
