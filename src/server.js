bb78583d3d2cb1268afa8cd6db9b1f0ff832ed46
David Pounds <david@davidpounds.com>
2021-02-16 22:22:24 +0000

    Adds backend server with websockets

---
+++ src/server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const path = require('path');
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/ping', (req, res) => res.send('pong'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', ws => {
+
+  ws.isAlive = true;
+
+  ws.on('pong', () => {
+    ws.isAlive = true;
+  });
+
+  ws.on('message', message => {
+    console.log('Received: %s', message);
+    const data = JSON.parse(message);
+    if (data?.broadcast === true) {
+      wss.clients.forEach(client => {
+        if (client !== ws) {
+          client.send(message);
+        }
+      });
+    } else {
+      ws.send(`You sent ${message}`);
+    }
+  });
+  ws.send('Connected to websocket');
+});
+
+setInterval(() => {
+  wss.clients.forEach(ws => {
+    if (!ws.isAlive) {
+      return ws.terminate();
+    }
+    
+    ws.isAlive = false;
+    ws.ping(null, false, true);
+  });
+}, 10000);
+
+server.listen(process.env.PORT || 8080, () => {
+  console.log(`Server started on port ${server.address().port}`);
+});
