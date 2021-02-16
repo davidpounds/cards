620762abf22e4c9c6a0f818697537c2fbe7f61d0
David Pounds <david@davidpounds.com>
2021-02-16 14:28:12 +0000

    Prevents cards being moved from in play to played until all players have played

--- src/components/InPlay.jsx
+++ src/components/InPlay.jsx
@@ -1,23 +1,29 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { getInPlayCards } from '../store/selectors';
+import { getInPlayCards, getPlayers } from '../store/selectors';
 import { addCardsToPlayed } from '../store/actions';
 import Card from './Card';
 
 const InPlay = props => {
   const dispatch = useDispatch();
   const inPlay = useSelector(getInPlayCards);
+  const players = useSelector(getPlayers);
+  const allPlayersHavePlayed = inPlay?.length === players?.length;
   const addCardsToPlayedHandler = () => {
-    dispatch(addCardsToPlayed(inPlay));
+    if (allPlayersHavePlayed) {
+      dispatch(addCardsToPlayed(inPlay));
+    }
   };
   return <>
     <h2>In play</h2>
     {inPlay?.length > 0 &&
-      <div className="cardlist inplay">
-        {inPlay.map(card => <Card key={`${card.suit}${card.value}`} suit={card.suit} value={card.value} />)}
-      </div>
+      <>
+        <div className="cardlist inplay">
+          {inPlay.map(card => <Card key={`${card.suit}${card.value}`} suit={card.suit} value={card.value} />)}
+        </div>
+        <button onClick={addCardsToPlayedHandler} disabled={!allPlayersHavePlayed}>Move to played</button>
+      </>
     }
     {(inPlay?.length ?? 0) === 0 && <div>No player has played a card</div>}
-    {inPlay?.length > 0 && <button onClick={addCardsToPlayedHandler}>Move to played</button>}
   </>;
 };
 
