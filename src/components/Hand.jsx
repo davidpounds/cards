9bbc5b8a155efaece6a38b859b355b168cd2a856
David Pounds <david@davidpounds.com>
2021-02-16 10:56:07 +0000

    Prevents user playing multiple cards per round

--- src/components/Hand.jsx
+++ src/components/Hand.jsx
@@ -1,21 +1,25 @@
 import './Hand.css';
 import { useSelector, useDispatch } from 'react-redux';
-import { getPlayerCards } from '../store/selectors';
+import { getPlayerCards, getInPlayCards } from '../store/selectors';
 import { addCardToInPlay } from '../store/actions';
 import Card from './Card';
 
 const Hand = props => {
-  const dispatch = useDispatch();
   const { player } = props;
+  const dispatch = useDispatch();
   const hand = useSelector(getPlayerCards(player));
+  const inPlay = useSelector(getInPlayCards);
+  const canPlay = inPlay.find(card => card.player === player) === undefined;
 
   const addToInPlay = card => () => {
-    dispatch(addCardToInPlay(card));
+    if (canPlay) {
+      dispatch(addCardToInPlay(card));
+    }
   };
 
   return <section className="player">
     <h2>{player}</h2>
-    <div className="cardlist hand">
+    <div className={`cardlist hand ${canPlay ? 'canplay' : 'cantplay'}`}>
       {hand.map(card => (
         <Card 
           key={`${card.suit}${card.value}`} 
