0bdf56f73be918e21f71de9b6b6c6ef4d823495e
David Pounds <david@davidpounds.com>
2021-02-17 21:16:47 +0000

    Gets server running and adds npm run dev command

--- server/index.js
+++ server/index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', (req, res) => res.send('pong'));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
